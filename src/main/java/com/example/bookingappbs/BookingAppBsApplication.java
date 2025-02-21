21835bf2294276019ed3baccdb43b16634d1a5df
Bohdan Shevtsov <shevtsov.b@gmail.com>
2025-02-21 11:29:22 +0200

    Blank

---
+++ src/main/java/com/example/bookingappbs/BookingAppBsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingappbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppBsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppBsApplication.class, args);
+    }
+
+}
